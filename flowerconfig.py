901cbb4e9f3c19c9c15d8dd65be24e98d44248d1
Nahidujjaman Hridoy <75487507+nhridoy@users.noreply.github.com>
2022-10-23 09:37:31 +0000

    Update flowerconfig.py

--- flowerconfig.py
+++ flowerconfig.py
@@ -1,9 +1,7 @@
 import os
 
-try:
-  broker_api = os.getenv("RABBIT_API")
-except:
-  broker = os.getenv("RABBIT_URL", os.getenv("REDIS_URL"))
+broker_api = os.getenv("RABBIT_API")
+broker = os.getenv("RABBIT_URL", os.getenv("REDIS_URL"))
 
 # Enable debug logging
 logging = os.getenv("FLOWER_LOGGING_LEVEL")
