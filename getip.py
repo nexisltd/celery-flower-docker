e2f966060c737192b3bf4631f6e2474f56a3fd34
Nahidujjaman Hridoy <75487507+nhridoy@users.noreply.github.com>
2022-11-10 18:22:19 +0600

    Update getip.py

--- getip.py
+++ getip.py
@@ -1,10 +1,10 @@
 from nslookup import Nslookup
 import os
 dns_query = Nslookup()
-
+print(os.getenv('BROKER_URL'))
 a = dns_query.dns_lookup(os.getenv('BROKER_URL').split(':')[2].split("@")[1])
 
 with open('.dockerenv', 'a') as f:
-    f.write(f'BROKER_API={a.answer[0]}')
-    f.write(f"BROKER_USER={os.getenv('BROKER_URL').split(':')[1].split('/')[2]}")
-    f.write(f"BROKER_PASS={os.getenv('BROKER_URL').split(':')[2].split('@')[0]}")
+    f.write(f'BROKER_API={a.answer[0]}\n')
+    f.write(f"BROKER_USER={os.getenv('BROKER_URL').split(':')[1].split('/')[2]}\n")
+    f.write(f"BROKER_PASS={os.getenv('BROKER_URL').split(':')[2].split('@')[0]}\n")
